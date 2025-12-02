b5765fd7f513e4c4aca280ab652275bc6b8ef665
loatheall <140192482+loatheall@users.noreply.github.com>
2025-12-02 18:19:24 +0100

    added ASCII art, fixed logic

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+from art import start
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
 again = False
 
-
-# TODO-1: Combine the encrypt() and decrypt() into a single function 'caesar()'
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use
-#  Call the caesar function instead of encrypt/decrypt and pass all 3 variables direction/text/shift
 def caesar(encode_or_decode, original_text, shift_amount):
     cipher_text = ""
 
@@ -25,17 +23,20 @@ def caesar(encode_or_decode, original_text, shift_amount):
 
 
 while not again:
+    print(start)
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n ").lower()
     if direction != "encode" and direction != "decode":
         print("Invalid direction")
-        continue
-    text = input("Type your message:\n ").lower()
-    shift = int(input("Type the shift number:\n "))
 
-    caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
+    else:
+        text = input("Type your message:\n ").lower()
+        shift = int(input("Type the shift number:\n "))
+
+        caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
+
 
     choice = input("Do you want to continue? (y/n): ").lower()
 
     if choice == "n":
         again = True
-        print("Thank you for playing!")
+        print(f"{start}\nThank you for playing!")
