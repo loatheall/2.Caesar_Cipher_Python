5fc6e970d56558ff0dd674b10c5121fae665274a
loatheall <140192482+loatheall@users.noreply.github.com>
2025-12-02 17:24:40 +0100

    added decrypt function

--- main.py
+++ main.py
@@ -4,25 +4,32 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n ").lowe
 text = input("Type your message:\n ").lower()
 shift = int(input("Type the shift number:\n "))
 
-
-
-# TODO-1: Create a function 'encrypt()' that takes "original_text" and 'shift_amount' as 2 inputs
 def encrypt(original_text, shift_amount):
-
     cipher_text = ""
 
-# TODO-2: Inside encrypt() function, shift each letter of the 'original_text' and shift each letter in a alphabet-
-# TODO-2: by the shift amount and print the encrypted text use index()
     for i in range(len(original_text)):
 
         number = alphabet.index(original_text[i])
         index_after_shift = number + shift_amount
 
-        # TODO-4: What happens if you try to shift z, can you fix the code?
         index_after_shift %= len(alphabet)
 
         cipher_text += alphabet[index_after_shift]
         print(f"Here is the encoded result: {cipher_text}")
 
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs
-encrypt(original_text=text, shift_amount=shift)
+# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs
+def decrypt(original_text, shift_amount):
+    cipher_text = ""
+
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' backwards in the alphabet
+#  by the 'shift_amount' and print the decrypted text
+    for i in range(len(original_text)):
+
+        number = alphabet.index(original_text[i])
+        index_after_shift = number - shift_amount
+
+        index_after_shift %= len(alphabet)
+
+        cipher_text += alphabet[index_after_shift]
+    print(f"Here is the decoded result: {cipher_text}")
+decrypt(original_text=text, shift_amount=shift)
